3a7f3f3c9df9456d3c6b10b3c4d66a17ff431360
brenicio <brenicio_guilherme2@hotmail.com>
2020-06-20 13:13:24 -0300

    commit inicial dos desafios

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafios LaunchBase
+
+>Desafios launchbase para refinar as habilidades em javascript, css e outras tecnologias.
---
+++ desafio1-1/imc.js
@@ -0,0 +1,11 @@
+const nome = "Carlos"
+const peso = 84
+const altura = 1.88
+
+const imc = peso / (altura * altura)
+
+if(imc >= 30){
+    console.log("Carlos você esta acima do peso!")
+}else if(imc <= 29.9){
+    console.log("Carlos você não está acima do peso!")
+}
